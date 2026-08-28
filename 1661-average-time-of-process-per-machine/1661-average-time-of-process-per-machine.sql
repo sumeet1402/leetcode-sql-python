4ccad066b076131dd39acf7c69ce72b6e65ca155
Sumeet Mandhre <162686757+sumeet1402@users.noreply.github.com>
2026-08-28 12:22:16 +0530

    Time: 231 ms (97.12%), Space: 0B (100%) - LeetHub

---
+++ 1661-average-time-of-process-per-machine/1661-average-time-of-process-per-machine.sql
@@ -0,0 +1,15 @@
+# Write your MySQL query statement below
+SELECT
+    machine_id,
+    ROUND(AVG(processing_time), 3) AS processing_time
+FROM (
+    SELECT
+        machine_id,
+        process_id,
+        MAX(CASE WHEN activity_type = 'end' THEN timestamp END)
+        -
+        MAX(CASE WHEN activity_type = 'start' THEN timestamp END) AS processing_time
+    FROM Activity
+    GROUP BY machine_id, process_id
+) AS process_times
+GROUP BY machine_id;
